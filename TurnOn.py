f9b565f16e4725ee1d2639f3cbcd11f3a704811c
Bryn Mathias <brynmathias@gmail.com>
2011-12-14 09:08:13 +0100

    break out the turn on and the get sum hist methods

---
+++ TurnOn.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+turnOnCurves.py
+
+Created by Bryn Mathias on 2011-12-10.
+Copyright (c) 2011 Imperial College. All rights reserved.
+"""
+
+import sys
+import os
+import ROOT as r
+import math
+
+class TurnOn(object):
+  """Turn on curve producer"""
+  def __init__(self, numerator = None, denominator = None):
+    super(TurnOn, self).__init__()
+    self.denominator = denominator
+    self.numerator = numerator
+    self.weights = None
+    self.listOfTurnOns = []
+    self.Approximate = False
+    self.finalPlot = None
+    self.Debug = False
+    self.TotNumerator = None
+    self.TotDenominator = None
+    self.ListOfTurnOns()
+    self.MakeTotalHists()
+  def ListOfTurnOns(self):
+    """docstring for ListOfTurnOns"""
+    i = 0
+    for nom,denom in zip(self.numerator,self.denominator):
+      if self.Approximate is not True:
+        graph = r.TGraphAsymmErrors()
+        graph.Divide(nom,denom)
+        self.listOfTurnOns.append(graph)
+      if self.Approximate is True:
+        a = nom.Clone()
+        if self.weights is not None:
+          a.Scale(weights[i])
+        a.Divde(denom)
+        graph = r.TGraph(a)
+        self.listOfTurnOns.append(graph)
+      i+=1
+    return self.listOfTurnOns
+    pass
+
+  def SumOfTurnOns(self):
+    """docstring for SumOfTurnOns"""
+    self.ListOfTurnOns()
+    self.finalPlot = r.TGraphAsymmErrors()
+    self.finalPlot.Divide(self.TotNumerator,self.TotDenominator)
+    for bin in range(self.denominator[0].GetNbinsX()):
+      point = bin - 1
+      sum_w_i = 0.
+      sum_w_i_plus = 0.
+      sum_w_i_minus = 0.
+      sum_eff_times_w_i = 0.
+      xVal = r.Double(0)
+      self.finalPlot.GetPoint(point,xVal,r.Double(0))
+      if self.Debug is True:
+        print "="*25
+        print "Start Bin %f, %f, binWidth = %f "%(self.TotDenominator.GetBinLowEdge(bin),xVal-(self.TotDenominator.GetBinWidth(1)/2.),self.TotDenominator.GetBinWidth(1) )
+        print "="*25
+
+
+      for TurnOn,denominator in zip(self.listOfTurnOns,self.denominator):
+        efficiency = r.Double(0)
+        xvalAtPoint = r.Double(0)
+        w_i = 0.
+        ErrorYhigh = TurnOn.GetErrorYhigh(point)
+        ErrorYlow = TurnOn.GetErrorYlow(point)
+        TurnOn.GetPoint(point,xvalAtPoint,efficiency)
+        if denominator.GetBinContent(bin) > 0.:
+          if ErrorYhigh**2 > 0.:  w_i_plus = 1./(ErrorYhigh**2)
+          else: w_i_plus = 0.
+          sum_w_i_plus += w_i_plus
+          if ErrorYlow**2 > 0. :w_i_minus = 1./(ErrorYlow**2)
+          else: w_i_minus = 0.
+          sum_w_i_minus += w_i_minus
+          w_i = max(w_i_minus,w_i_plus)
+          sum_w_i += w_i
+          sum_eff_times_w_i += (efficiency*w_i)
+          if self.Debug is True:
+            print " w_i_plus = %f, w_i_minus = %f, sum_w_i %f, Efficiency = %f, efficiency*w_i = %f, at x = %f"%(w_i_plus,w_i_minus,sum_w_i, efficiency, efficiency*w_i, xvalAtPoint)
+
+      if sum_w_i > 0.:
+        AvEff = sum_eff_times_w_i/sum_w_i
+      else : AvEff = 0.
+
+      if sum_w_i_minus > 0.:
+        error_minus = math.sqrt(1./sum_w_i_minus)
+      else: error_minus = 0.
+      if sum_w_i_plus > 0.:
+        error_plus = math.sqrt(1./sum_w_i_plus)
+      else: error_plus = 0.
+
+      self.finalPlot.SetPoint(point,xVal,AvEff)
+      if AvEff+error_plus > 1.:
+        error_plus = 1.-AvEff
+      # if AvEff-error_minus < 1.:
+      #   error_minus = AvEff - 1
+      self.finalPlot.SetPointError(point,self.finalPlot.GetErrorXlow(point),self.finalPlot.GetErrorXhigh(point),error_minus,error_plus )
+    return self.finalPlot
+    pass
+
+  def MakeTotalHists(self):
+    """docstring for TotalNumerator"""
+    for hist in self.numerator:
+      if self.TotNumerator is None:
+        self.TotNumerator = hist.Clone()
+      else: self.TotNumerator.Add(hist)
+    for hist in self.denominator:
+      if self.TotDenominator is None:
+        self.TotDenominator = hist.Clone()
+      else:self.TotDenominator.Add(hist)
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+  pass
+
+
+if __name__ == '__main__':
+  main()
+
--- plottingUtils.py
+++ plottingUtils.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-untitled.py
+PlottingUtils.py
 
 Created by Bryn Mathias on 2011-09-30.
-Copyright (c) 2011 Imperial College. All rights reserved.
 """
 
-import sys
-import os
-import unittest
-import ROOT as r
 
-#!/usr/bin/env python
 
-'''Created by Bryn Mathias
-bryn.mathias@cern.ch
-'''
-import errno
 
-import ROOT as r
-import math
-from time import strftime
-import os, commands
-import array
+def MakeCumu(inHist):
+    cumulativeHist = inHist.Clone()
+    maxbin = inHist.GetNbinsX()
+    for bin in range(0,maxbin):
+      err = r.Double(0)
+      val = inHist.IntegralAndError(bin, maxbin, err)
+      cumulativeHist.SetBinContent(bin,val)
+      cumulativeHist.SetBinError(bin,err)
+    return cumulativeHist
+
+
+
 def ensure_dir(path):
     try:
       os.makedirs(path)
@@ -199,18 +196,18 @@ class Print(object):
     self.canvas = r.TCanvas()
 
     self.fname = Fname
-    self.rfile = r.TFile(self.fname[:-4]+".root",'RECREATE')
+    # self.rfile = r.TFile(self.fname[:-4]+".root",'RECREATE')
     self.pageCounter = 1
 
 
 
   def toFile(self,ob,title):
     """docstring for toFile"""
-    self.rfile.cd()
-    ob.SetName(title)
-    ob.SetTitle(title)
-    ob.Write()
-    ob = None
+    # self.rfile.cd()
+    # ob.SetName(title)
+    # ob.SetTitle(title)
+    # ob.Write()
+    # ob = None
     pass
 
   def cd(self):
@@ -227,8 +224,8 @@ class Print(object):
 
   def close(self):
     """docstring for close"""
-    self.rfile.Write()
-    self.rfile.Close()
+    # self.rfile.Write()
+    # self.rfile.Close()
     self.canvas.Print(self.fname+"]")
     pass
 
@@ -265,195 +262,7 @@ class Print(object):
     pass
 
 
-class TurnOn(object):
-  """docstring for TurnOn"""
-  def __init__(self, Numerator, Denominator):
-    self.nom = Numerator
-    self.denom = Denominator
-    self.Title = None
-    self.xaxisTitle = None
-    self.yaxisTitle = None
-    self.leg = Legend()
-    self.TGraph = r.TGraphAsymmErrors()
-    self.xmin = None
-    self.ymin = -0.2
-    self.xmax = None
-    self.ymax = 1.2
-    self.nomClone  = None
-    self.denomClone = None
-    self.newBins = None
-    self.text90 = None
-    self.text95 = None
-    self.text99 = None
-    self.mg = r.TMultiGraph()
-    self.tgraphlist = []
-    self.nomClones = []
-    self.denomClones = []
-    self.finalGraph = None
-    self.x = []
-    self.y = []
-    self.errxup = []
-    self.errxlow = []
-    self.erryup = []
-    self.errylow = []
-    self.Hist = r.TH1D()#Numerator.hObj.Clone()
-    # self.DivHist = Denominator.hObj.Clone()
-    self.UseTgraph = True
 
-  def setRange(self,x1,x2):
-    """docstring for SetRange"""
-    self.xmin = x1
-    self.xmax = x2
-    pass
-
-  def DifferentialTurnOn(self):
-    if self.UseTgraph == True:
-      """docstring for DifferentialTurnOn"""
-      self.TGraph.Divide(self.nom.hObj,self.denom.hObj)
-      self.TGraph.GetXaxis().SetTitle(self.nom.hObj.GetXaxis().GetTitle())
-      self.TGraph.GetXaxis().SetTitleSize(0.05)
-      self.TGraph.GetYaxis().SetTitle("Efficiency")
-      self.TGraph.GetXaxis().SetRangeUser(self.xmin,self.xmax)
-      self.TGraph.GetYaxis().SetRangeUser(self.ymin,self.ymax)
-      self.TGraph.SetTitle("Differential turn on for %s, from file: %s"%(self.nom.directories,self.nom.files))
-      self.TGraph.SetName("Differential turn on for %s, from file: %s"%(self.nom.directories,self.nom.files))
-      return self.TGraph
-    if self.UseTgraph == False:
-      # self.Hist = self.nom.hObj.Clone()
-      # self.Hist.Scale(self.nom.norm[0])
-      self.Hist.Divide(self.nom.hObj,self.denom.hObj,self.nom.norm[0], 1.0,"")
-      self.Hist.GetXaxis().SetTitleSize(0.05)
-      self.Hist.GetYaxis().SetTitle("Efficiency")
-      # self.Hist.GetXaxis().SetRangeUser(self.xmin,self.xmax)
-      # self.Hist.GetYaxis().SetRangeUser(self.ymin,self.ymax)
-      self.Hist.SetTitle("Differential turn on for %s, from file: %s"%(self.nom.directories,self.nom.files))
-      self.Hist.SetName("Differential turn on for %s, from file: %s"%(self.nom.directories,self.nom.files))
-      return self.Hist
-  def setGraph(self,sethist_):
-    """docstring for setType"""
-    self.UseTgraph = sethist_
-    pass
-
-  def RobPlot(self):
-    def binomialErr(nom,denom):
-      p = 0.5
-      q = 1 - p
-      nk = math.factorial(denom)/(math.factorial(nom) * math.factorial( denom - nom))
-      print pow(p,nom) , pow(q,(denom - nom)), nk
-      if pow(p,nom) == 0 or pow(q,(denom - nom)) == 0.: return 0
-      pHat = nk * pow(p,nom) * (pow(q,(denom - nom)))
-      return 1.96*math.sqrt((pHat*(1-pHat))/denom)
-      pass
-    """docstring for RobPlot"""
-    self.tgraphlist = []
-    binWidth = self.nom.hObj.GetBinWidth(1)
-    maxi = self.nom.hObj.GetBinWidth(1) *self.nom.hObj.GetNbinsX()
-    maxbin = self.nom.hObj.GetNbinsX()
-    for bin in range(1,self.nom.hObj.GetNbinsX()):
-      if bin * binWidth > maxi: continue
-      nomErr = r.Double(0)
-      denomErr = r.Double(0)
-      binNom =  self.nom.hObj.IntegralAndError(bin, maxbin, nomErr)
-      binDenom =  self.denom.hObj.IntegralAndError(bin, maxbin, denomErr)
-      if binDenom != 0:
-        print "Bin Center = %f , Efficiency = %f"%(self.nom.hObj.GetBinCenter(bin),binNom/binDenom)
-        self.x.append(self.nom.hObj.GetBinCenter(bin))
-        self.y.append(binNom/binDenom)
-        self.errxup.append((binWidth/2.))
-        self.errxlow.append(-(binWidth/2.))
-        self.erryup.append(0)
-        self.errylow.append(binomialErr(binNom,binDenom))
-    self.finalGraph =   r.TGraphAsymmErrors(len(self.x)-1, array.array('d',self.x), array.array('d',self.y), array.array('d',self.errxlow), array.array('d',self.errxup), array.array('d',self.errylow), array.array('d',self.erryup))
-    self.finalGraph.SetMarkerStyle(20)
-    self.finalGraph.SetMarkerSize(0.5)
-    self.finalGraph.SetTitle("%s (pre = %s) from  %s (pre = %s) "%((self.nom.hist.split("_")[2]),(self.nom.hist.split("_")[4]),(self.nom.hist.split("_")[6]),(self.nom.hist.split("_")[-1])))
-    return self.finalGraph
-
-
-
-
-
-  def CumuMultiGraph(self):
-    """docstring for CumuMultiGraph"""
-    triggerExpected = float((self.nom.directories[0].split("_")[1])[2:])
-    print triggerExpected
-    binWidth = self.nom.hObj.GetBinWidth(1)
-    self.tgraphlist = []
-    maxi = self.nom.hObj.GetNbinsX() * binWidth
-    for bin in range(0,50):
-      self.nomClones.append(self.nom.hObj.Clone())
-      self.denomClones.append(self.denom.hObj.Clone())
-
-      print self.newBins
-
-      a = r.TGraphAsymmErrors()
-      self.tgraphlist.append(a)
-      self.tgraphlist[bin].SetName("i")
-      self.tgraphlist[bin].Divide(self.nomClones[bin].Rebin(  len(self.newBins)-1,"a"+str(bin), self.newBins),self.denomClones[bin].Rebin(len(self.newBins)-1,"b"+str(bin), self.newBins))
-      # a.GetXaxis().SetRangeUser()
-      self.tgraphlist[bin].SetLineColor(bin+1)
-      self.tgraphlist[bin].SetMarkerColor(bin+1)
-      # self.tgraphlist.append(a)
-      xval = r.Double(0)
-      yval = r.Double(0)
-      self.tgraphlist[bin].GetPoint(1,xval,yval)
-      self.tgraphlist[bin].SetTitle("%f"%(triggerExpected + (bin * binWidth)))
-      self.tgraphlist[bin].SetTitle("%f , %f"%(triggerExpected + (bin * binWidth),yval))
-      print "yval = %f, bin = %d, bin lowEdge = %d"%(yval,bin,triggerExpected + (bin * binWidth))
-      self.leg.AddEntry(self.tgraphlist[bin],"Eff = %f, Cut = %f"%(yval,triggerExpected + (bin * binWidth)),"l")
-      print self.nom.hist
-      if yval == 1.0:
-        self.mg.SetTitle("%s (pre = %s) from  %s (pre = %s) "%((self.nom.hist.split("_")[2]),(self.nom.hist.split("_")[4]),(self.nom.hist.split("_")[6]),(self.nom.hist.split("_")[-1])))
-        break
-
-    for graph in self.tgraphlist:
-      self.mg.Add(graph)
-    return (self.mg , self.leg)
-    pass
-
-
-  def CumulativeTurnOn(self,Bins):
-    """docstring for CumulativeTurnOn"""
-    self.TGraph = r.TGraphAsymmErrors()
-    self.newBins = array.array('d',Bins)
-    self.nomClone  = self.nom
-    self.denomClone = self.denom
-    self.nomClone.Rebin(  len(Bins)-1, self.newBins)
-    self.denomClone.Rebin(len(Bins)-1, self.newBins)
-    self.TGraph.Divide(self.nomClone.hObj,self.denomClone.hObj)
-    yval = r.Double(0)
-    xval = r.Double(0)
-    self.TGraph.GetPoint(1,xval,yval)
-    self.TGraph.SetTitle("Cumulative turn on for above plot, with a cut of %f, Efficiency is %f + %f - %f"%(Bins[1],yval,self.TGraph.GetErrorYhigh(1),self.TGraph.GetErrorYlow(1)))
-    self.TGraph.GetYaxis().SetRangeUser(self.ymin,self.ymax)
-    self.TGraph.GetXaxis().SetTitle(self.nom.hObj.GetXaxis().GetTitle())
-    self.TGraph.GetXaxis().SetTitleSize(0.05)
-    return self.TGraph
-
-  def logEffs(self):
-    """docstring for logEffs"""
-    yval = r.Double(0)
-    xval = r.Double(0)
-    self.text90 = r.TLatex(0.01,0.75,"")
-    self.text95 = r.TLatex(0.01,0.80,"")
-    self.text99 =  r.TLatex(0.01,0.80,"")
-    maxBinEdge = self.nom.hObj.GetBinLowEdge(self.nom.hObj.GetNbinsX())
-    for bin in range(1,self.nom.hObj.GetNbinsX()-1):
-      TGraph = r.TGraphAsymmErrors()
-      newBinEdge = self.nom.hObj.GetBinWidth(1) * bin
-      nomClone = self.nom.hObj.Rebin(2,"nomClone",array.array('d',[0.,newBinEdge,maxBinEdge]))
-      denomClone = self.denom.hObj.Rebin(2,"denomClone",array.array('d',[0.,newBinEdge,maxBinEdge]))
-      TGraph.Divide(nomClone,denomClone)
-      TGraph.GetPoint(1,xval,yval)
-      # print "approx %f efficient at a cut of %f"%(yval,newBinEdge)
-      if yval > 0.90 and yval < 0.95: self.text90 = r.TLatex(0.01,0.75,"%s is approx 90%% efficient at a cut of %f"%(self.nom.hist.split("_")[0],newBinEdge))
-      if yval > 0.95:
-        self.text95 = r.TLatex(0.01,0.80,"%s is approx 95%% efficient at a cut of %f"%(self.nom.hist.split("_")[0],newBinEdge))
-        if yval > 0.99:
-          self.text99 = r.TLatex(0.01,0.85,"%s is >= 99%% efficient  at a cut of %f"%(self.nom.hist.split("_")[0],newBinEdge))
-          break
-    return [self.text90,self.text95,self.text99]
-    pass
 
 class TurnOnPrefs(object):
   """docstring for TurnOnPrefs"""
